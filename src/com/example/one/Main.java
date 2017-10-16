6bf59dcef1b99186a08feb01f1094c7e2cf3ec3a
Marcin Paluchowski <victorarmy@gmail.com>
2017-10-16 10:36:10 +0200

    Issue resolved.

--- src/com/example/one/Main.java
+++ src/com/example/one/Main.java
@@ -8,22 +8,27 @@ public class Main {
         List<Integer> userInputs = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
-        int userInput = 0;
-        while (userInput >= 0) {
-            System.out.println("Podaj liczbę: ");
-            userInput = scanner.nextInt();
-            userInputs.add(userInput);
-        }
+        try {
+            int userInput;
+            System.out.println("Podaj liczbe");
+            while ((userInput = scanner.nextInt()) >= 0) {
+                userInputs.add(userInput);
+                System.out.println("Podaj liczbę");
+            }
 
-        Collections.reverse(userInputs);
-        System.out.println(userInputs);
-        int sum = 0;
-        for (Integer input : userInputs) {
-            sum += input;
+            Collections.reverse(userInputs);
+            System.out.println(userInputs);
+            int sum = 0;
+            for (Integer input : userInputs) {
+                sum += input;
+            }
+            System.out.println(sum);
+            System.out.println(Collections.min(userInputs));
+            System.out.println(Collections.max(userInputs));
+        } catch (InputMismatchException e) {
+            System.err.println("Podałeś złe dane. Zamykanie.");
+            e.printStackTrace();
         }
-        System.out.println(sum);
-        System.out.println(Collections.min(userInputs));
-        System.out.println(Collections.max(userInputs));
         scanner.close();
     }
 }
